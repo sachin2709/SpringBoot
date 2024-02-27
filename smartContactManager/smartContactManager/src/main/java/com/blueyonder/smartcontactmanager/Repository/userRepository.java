2433eb6f879fc71df25f310c46cd4d888685f75d
Sachin Mishra <96466673+sachin2709@users.noreply.github.com>
2024-02-28 00:15:32 +0530

    Delete smartContactManager/smartContactManager directory

--- smartContactManager/smartContactManager/src/main/java/com/blueyonder/smartcontactmanager/Repository/userRepository.java
+++
@@ -1,8 +0,0 @@
-package com.blueyonder.smartcontactmanager.Repository;
-
-import com.blueyonder.smartcontactmanager.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userRepository extends JpaRepository<User,Integer> {
-
-}
